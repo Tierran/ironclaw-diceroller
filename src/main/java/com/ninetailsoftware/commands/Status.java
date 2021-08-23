125ddd6b28beed21243c8494276fca29f0a78063
David Tucker <drgnfox@gmail.com>
2021-08-23 14:12:38 -0500

    Cleaned up some code around displaying status

--- src/main/java/com/ninetailsoftware/commands/Status.java
+++ src/main/java/com/ninetailsoftware/commands/Status.java
@@ -17,11 +17,11 @@ public class Status {
 
 	public void statusCommand(String messageContent, TextChannel channel) {
 		// Format should be !status <command> <who> <status>, <status>, ...
-		
+
 		messageContent = messageContent.toLowerCase();
-		
+
 		String command = messageContent.split(" ")[1];
-				
+
 		switch (command) {
 		case "add":
 			addStatus(messageContent);
@@ -40,7 +40,7 @@ public class Status {
 			break;
 		}
 	}
-	
+
 	private void dropCharacter(String messageContent) {
 		String characterName = this.capitalizeAll(messageContent.split(" ")[2]);
 		characters.remove(characterName);
@@ -48,14 +48,21 @@ public class Status {
 
 	private void addStatus(String messageContent) {
 		String characterName = messageContent.split(" ")[2];
+		String characterType = messageContent.split(" ")[3];
 		messageContent = messageContent.substring(messageContent.indexOf(" ", messageContent.indexOf(characterName)))
 				.trim();
 
 		characterName = this.capitalizeAll(characterName);
 
+		if (characterType.toUpperCase().contains("PC")) {
+			messageContent = messageContent.replace(characterType, "").trim();
+		}
+
 		ArrayList<String> statuses = new ArrayList<String>();
-		for (int i = 0; i < messageContent.split(",").length; i++) {
-			statuses.add(messageContent.split(",")[i].trim());
+		if (messageContent.length() > 0) {
+			for (int i = 0; i < messageContent.split(",").length; i++) {
+				statuses.add(messageContent.split(",")[i].trim());
+			}
 		}
 
 		Character character;
@@ -66,18 +73,24 @@ public class Status {
 		} else {
 			character = characters.get(characterName);
 		}
+		if (characterType.toUpperCase().contains("PC")) {
+			character.setType(characterType.toUpperCase());
+		}
+
 		for (int i = 0; i < statuses.size(); i++) {
-			character.getStatus().add(this.capitalizeAll(statuses.get(i)));
+			if (!statuses.get(i).equals("")) {
+				character.getStatus().add(this.capitalizeAll(statuses.get(i)));
+			}
 		}
 	}
 
 	private void removeStatus(String messageContent) {
-		String characterName =messageContent.split(" ")[2];
+		String characterName = messageContent.split(" ")[2];
 		messageContent = messageContent.substring(messageContent.indexOf(" ", messageContent.indexOf(characterName)))
 				.trim();
 
 		characterName = this.capitalizeAll(characterName);
-		
+
 		ArrayList<String> statuses = new ArrayList<String>();
 		for (int i = 0; i < messageContent.split(",").length; i++) {
 			statuses.add(messageContent.split(",")[i].trim());
@@ -95,23 +108,44 @@ public class Status {
 	private void showAllStatus(TextChannel channel) {
 		MessageBuilder mb = new MessageBuilder();
 		ArrayList<String> keys = new ArrayList<String>(characters.keySet());
-		mb.append("PCs", MessageDecoration.BOLD).appendNewLine();
+
+		Boolean pc = false;
+		Boolean npc = false;
+		Boolean unknown = false;
+
 		for (int i = 0; i < keys.size(); i++) {
 			Character character = characters.get(keys.get(i));
-			if(character.getType() != null && character.getType().equals("PC"))
+			if (character.getType() != null && character.getType().equals("PC")) {
+				if (!pc) {
+					mb.append("PCs", MessageDecoration.BOLD).appendNewLine();
+					pc = true;
+				}
 				mb.append(character.getName() + " ").append(character.getStatus().toString()).appendNewLine();
+			}
 		}
-		mb.append("NPCs", MessageDecoration.BOLD).appendNewLine();
+
 		for (int i = 0; i < keys.size(); i++) {
 			Character character = characters.get(keys.get(i));
-			if(character.getType() != null && character.getType().equals("NPC"))
+			if (character.getType() != null && character.getType().equals("NPC")) {
+				if (!npc) {
+					mb.append("NPCs", MessageDecoration.BOLD).appendNewLine();
+					npc = true;
+				}
+
 				mb.append(character.getName() + " ").append(character.getStatus().toString()).appendNewLine();
+			}
 		}
-		mb.append("UNKNOWN:", MessageDecoration.BOLD).appendNewLine();
+
 		for (int i = 0; i < keys.size(); i++) {
 			Character character = characters.get(keys.get(i));
-			if(character.getType() == null)
+			if (character.getType() == null) {
+				if (!unknown) {
+					mb.append("NPCs", MessageDecoration.BOLD).appendNewLine();
+					unknown = true;
+				}
+
 				mb.append(character.getName() + " ").append(character.getStatus().toString()).appendNewLine();
+			}
 		}
 		mb.send(channel);
 	}
@@ -137,12 +171,12 @@ public class Status {
 		} else {
 			character = characters.get(userName);
 		}
-		
-		if(status != null) {
+
+		if (status != null) {
 			character.getStatus().add(status);
 		}
 	}
-	
+
 	private void resetStatus() {
 		characters.clear();
 	}
